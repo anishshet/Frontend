034e1f3485bad334b4989e1f45649ec9f29ff6d1
Anishshet <anishshet5@gmail.com>
2025-05-15 17:27:41 +0530

    merged

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthLayout } from './layouts/AuthLayout';
 import { LoginPage } from './pages/LoginPage';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import ForgotPasswordPage from './pages/ForgotPasswordPage';
-import { UpdatePassword } from './pages/UpdatePassword';
-
+import { ClientsPage } from './pages/ClientsPage';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
@@ -18,19 +17,10 @@ function App() {
         <Route element={<AuthLayout />}>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/clients" element={<ClientsPage />} />
+          <Route path='/forgot-password' element={<ForgotPasswordPage/>} />
         </Route>
       </Routes>
     </Router>
-    <div className="w-full h-full">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Navigate to="/login" replace />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-          
-        </Routes>
-      </BrowserRouter>
-    </div>
   );
 }
 
